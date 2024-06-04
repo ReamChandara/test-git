79d9adbffb9310d9d7ec65a097ec01bce8ab93bc
unknown <reamchandara2020@gmail.com>
2024-06-04 16:34:19 +0700

    update-dev-branch

--- lib/home/new_view_page.dart
+++ lib/home/new_view_page.dart
@@ -7,7 +7,7 @@ class NewViewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("New View Page"),
+        title: const Text("New View Page Edting "),
       ),
     );
   }
