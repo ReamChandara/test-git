6ea38d7b02b8114ecaa4062e92f28ccdac1e1f6c
unknown <reamchandara2020@gmail.com>
2024-06-04 16:17:54 +0700

    new-update-in-main

--- lib/home/new_home_page.dart
+++ lib/home/new_home_page.dart
@@ -7,7 +7,7 @@ class NewHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("New HomePage"),
+        title: const Text("New HomePage update in main branch"),
       ),
     );
   }
--- lib/widget/new_widget.dart
+++ lib/widget/new_widget.dart
@@ -5,6 +5,6 @@ class NewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Text("Update In main branch");
   }
 }
